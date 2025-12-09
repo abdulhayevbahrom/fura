374a7ce5d70c0e5abe5bd6dce16d5e5123644b1c
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-12-09 11:24:58 +0500

    last version

--- model/mapModel.js
+++ model/mapModel.js
@@ -1,10 +1,15 @@
 const { Schema, model } = require("mongoose");
 
-const mapSchema = new Schema({
-  driver: { type: Schema.Types.ObjectId, ref: "drivers", required: true },
-  lat: { type: Number, required: true },
-  long: { type: Number, required: true },
-  speed: { type: Number, required: true },
-});
+const mapSchema = new Schema(
+  {
+    driver: { type: Schema.Types.ObjectId, ref: "drivers", required: true },
+    lat: { type: Number, required: true },
+    long: { type: Number, required: true },
+    speed: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model("maps", mapSchema);
